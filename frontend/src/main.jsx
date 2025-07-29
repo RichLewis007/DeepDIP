570f38a27ca8090cc98cc3f63e9fc275b859e74e
Rich Lewis <1149213+RichLewis007@users.noreply.github.com>
2025-07-29 02:31:13 -0400

    Docker backend + compose file

---
+++ frontend/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return <div className="p-4">DeepDIP Frontend Coming Soon</div>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
